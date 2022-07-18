f1eed872e635c5eb8cb02a434840092b55be75f6
Klivinsthon Kendert <contato_klivinsthon@outlook.com>
2022-07-17 21:13:19 -0300

    Adicionada responsividade no header e iniciada no painel geral

--- src/components/interface/interface.js
+++ src/components/interface/interface.js
@@ -5,18 +5,26 @@ function Interface() {
     return (
         <Fragment>
             <header className="flex">
-                <div className="logo">DADOS COVID</div>
-                <div className="hidden sm:block titulo-cabecalho">Dados atualizados sobre o covid-19</div>
+                <div className="logo 
+                                w-4/12 sm:w-3/12 lg:w-2/12">DADOS COVID</div>
+                <div className="titulo-cabecalho hidden sm:block 
+                                lg:text-md sm:pt-2 lg:pt-3">
+                                    Dados atualizados sobre o covid-19
+                </div>
             </header>
             <div className="container mx-auto">
                 <div className="painel-geral
                                 mx-auto 
-                                w-9/12 md:8/12
+                                w-9/12 sm:w-8/12
                                 flex flex-wrap justify-evenly">
 
-                    <div className="a confirmados shrink-0	">a</div>
-                    <div className="a shrink-0	">a</div>
-                    <div className="a shrink-0	">a</div>
+                    <div className="shrink-0
+                                    h-32"></div>
+                    <div className="shrink-0 
+                                    h-32"></div>
+                    <div className="shrink-0 
+                                    h-32
+                                    m-1"></div>
                 </div>
             </div>
         </Fragment>
