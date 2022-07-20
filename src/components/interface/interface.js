0c30af3fa3271ca155f1df2fd09e715030a8f49e
Klivinsthon Kendert <contato_klivinsthon@outlook.com>
2022-07-20 15:45:40 -0300

    Dados inseridos nos blocos e responsividade definida

--- src/components/interface/interface.js
+++ src/components/interface/interface.js
@@ -18,23 +18,43 @@ function Interface() {
                                 w-11/12 sm:w-7/12 lg:w-10/12
                                 flex flex-wrap justify-evenly gap-y-3 gap-x-3">
 
-                    <div className="shrink-0
-                                    h-32 
-                                    w-60 sm:w-72 md:w-80
-                                    border">
-
-                        <h1 className="titulo confirmados
-                                        text-lg md:text-xl">CASOS CONFIRMADOS</h1>
-
+                    <div className="bloco-infos
+                                    shrink-0
+                                    w-60 sm:w-72 md:w-80">
+                        <h1 className="titulo titulo-confirmados
+                                       text-lg md:text-xl">CASOS CONFIRMADOS</h1>
+                        <div className="infos-detalhadas
+                                        sm:pl-4">
+                            <div className="text-sm"><span>Confirmados</span></div>
+                            <div className="font-black pl-1"><span>635.336.278</span></div>
+                            <div className="w-6/12 text-sm">Novos casos</div>
+                            <div className="font-black pl-1"><span>43.000</span></div>
+                        </div>
                     </div>
-                    <div className="shrink-0
-                                    h-32 
-                                    w-60 sm:w-72 md:w-80
-                                    border">a</div>
-                    <div className="shrink-0 
-                                    h-32 
-                                    w-60 sm:w-72 md:w-80
-                                    border">a</div>
+
+                    <div className="bloco-infos
+                                    shrink-0
+                                    w-60 sm:w-72 md:w-80">
+                        <h1 className="titulo titulo-obitos
+                                       text-lg md:text-xl">CASOS CONFIRMADOS</h1>
+                        <div className="infos-detalhadas
+                                        sm:pl-4">
+                            <div className="text-sm"><span>Confirmados</span></div>
+                            <div className="font-black pl-1"><span>635.336.278</span></div>
+                            <div className="flex pt-2">
+                                <div className="text-sm w-6/12">Novos óbitos</div>
+                                <div className="text-sm w-6/12"><span>Letalidade</span></div>
+                            </div>
+                            <div className="flex">
+                                <div className="font-black pl-1 w-6/12"> 215</div>
+                                <div className="font-black pl-1 w-6/12"><span>2,0%</span></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="bloco-infos
+                                    shrink-0 
+                                    w-60 sm:w-72 md:w-80">a</div>
                 </div>
             </div>
         </Fragment>
